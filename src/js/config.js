b2099ac78208495dc52f1faf872d935f5bba065a
Kostyan89 <kostasamin@gmail.com>
2023-06-04 10:20:58 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd4eba819-d370-4e9f-9f92-5b6d847efeb1';
+export const MODAL_CLOSE_SEC = 2.5;
